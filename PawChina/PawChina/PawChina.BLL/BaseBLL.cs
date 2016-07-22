dcde3f18c70f6a24d76f3d321fc1040dddf91add
dunitian <1054186320@qq.com>
2016-07-22 21:57:43 +0800

    IBaseBLL<T> and BaseBLL<T>

--- PawChina/PawChina/PawChina.BLL/BaseBLL.cs
+++ PawChina/PawChina/PawChina.BLL/BaseBLL.cs
@@ -1,10 +1,170 @@
-﻿using PawChina.IBLL;
+﻿using System.Data;
+using PawChina.IBLL;
 using PawChina.IDal;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace PawChina.BLL
 {
-    public class BaseBLL: IBaseBLL
+    public abstract partial class BaseBLL<T> : IBaseBLL<T> where T : class, new()
     {
-        protected IBaseDal baseDal = Factory.DalFactory.Resolve<IBaseDal>();
+        #region 获取Dal对象
+        protected IBaseDal<T> modelDal;
+        protected abstract IBaseDal<T> GetModelDal();//abstract~子类继承后必须实现
+        public BaseBLL()
+        {
+            modelDal = GetModelDal();
+        }
+        #endregion
+
+        #region 基础方法
+
+        #region 查询系列
+        /// <summary>
+        /// 单个值返回值
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="param"></param>
+        /// <param name="transaction"></param>
+        /// <param name="commandTimeout"></param>
+        /// <param name="commandType"></param>
+        /// <returns></returns>
+        public async Task<T> ExecuteScalarAsync(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = default(int?), CommandType? commandType = default(CommandType?))
+        {
+            return await modelDal.ExecuteScalarAsync(sql, param, transaction, commandTimeout, commandType);
+        }
+        /// <summary>
+        /// 强类型查询
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="param"></param>
+        /// <param name="transaction"></param>
+        /// <param name="buffered"></param>
+        /// <returns></returns>
+        public async Task<IEnumerable<T>> QueryAsync(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = default(int?), CommandType? commandType = default(CommandType?))
+        {
+            return await modelDal.QueryAsync(sql, param, transaction, commandTimeout, commandType);
+        }
+        /// <summary>
+        /// 动态类型查询 | 多映射动态查询
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="param"></param>
+        /// <param name="transaction"></param>
+        /// <param name="buffered"></param>
+        /// <returns></returns>
+        public async Task<IEnumerable<dynamic>> QueryAsyncDynamic(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = default(int?), CommandType? commandType = default(CommandType?))
+        {
+            return await modelDal.QueryAsync(sql, param, transaction, commandTimeout, commandType);
+        }
+        #endregion
+
+        #region 增删改系
+        /// <summary>
+        /// 增删改系
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="param"></param>
+        /// <param name="transaction"></param>
+        /// <param name="commandTimeout"></param>
+        /// <param name="commandType"></param>
+        /// <returns></returns>
+        public async Task<int> ExecuteAsync(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = default(int?), CommandType? commandType = default(CommandType?))
+        {
+            return await modelDal.ExecuteAsync(sql, param, transaction, commandTimeout, commandType);
+        }
+        #endregion
+
+        #endregion
+
+        #region 扩展方法
+
+        #region 查询系
+        /// <summary>
+        /// 获取Model-Key为int类型
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="transaction"></param>
+        /// <param name="commandTimeout"></param>
+        /// <returns></returns>
+        public async Task<T> GetAsync(int id, IDbTransaction transaction = null, int? commandTimeout = null)
+        {
+            return await modelDal.GetAsync(id, transaction, commandTimeout);
+        }
+        /// <summary>
+        /// 获取Model-Key为long类型
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="transaction"></param>
+        /// <param name="commandTimeout"></param>
+        /// <returns></returns>
+        public async Task<T> GetAsync(long id, IDbTransaction transaction = null, int? commandTimeout = null)
+        {
+            return await modelDal.GetAsync(id, transaction, commandTimeout);
+        }
+        /// <summary>
+        /// 获取Model-Key为Guid类型
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="transaction"></param>
+        /// <param name="commandTimeout"></param>
+        /// <returns></returns>
+        public async Task<T> GetAsync(System.Guid id, IDbTransaction transaction = null, int? commandTimeout = null)
+        {
+            return await modelDal.GetAsync(id, transaction, commandTimeout);
+        }
+        /// <summary>
+        /// 获取Model集合（没有Where条件）
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await modelDal.GetAllAsync();
+        }
+        #endregion
+
+        #region 增删改
+        /// <summary>
+        /// 插入一个Model
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="transaction"></param>
+        /// <param name="commandTimeout"></param>
+        /// <returns></returns>
+        public async Task<int> InsertAsync(T model, IDbTransaction transaction = null, int? commandTimeout = null)
+        {
+            return await modelDal.InsertAsync(model, transaction, commandTimeout);
+        }
+
+        /// <summary>
+        /// 更新一个Model
+        /// </summary>
+        /// <param name="connection"></param>
+        /// <param name="entityToUpdate"></param>
+        /// <param name="transaction"></param>
+        /// <param name="commandTimeout"></param>
+        /// <returns></returns>
+        public async Task<T> UpdateAsync(T model, IDbTransaction transaction = null, int? commandTimeout = null)
+        {
+            return await modelDal.UpdateAsync(model, transaction, commandTimeout);
+        }
+        #endregion
+
+        #region 分页查询
+        /// <summary>
+        /// 分页查询(为什么不用out，请参考：http://www.cnblogs.com/dunitian/p/5556909.html)
+        /// </summary>
+        /// <param name="sql">查询语句</param>
+        /// <param name="p">动态参数</param>
+        /// <param name="sqlTotal">total语句</param>
+        /// <param name="p2">Total动态参数</param>
+        /// <returns></returns>
+        public async Task<string> PageLoadAsync(string sql, object p = null, string sqlTotal = "", object p2 = null)
+        {
+            return await modelDal.PageLoadAsync(sql, p, sqlTotal, p2);
+        }
+        #endregion
+
+        #endregion
     }
 }
--- PawChina/PawChina/PawChina.IBLL/IBaseBLL.cs
+++ PawChina/PawChina/PawChina.IBLL/IBaseBLL.cs
@@ -1,7 +1,126 @@
-﻿namespace PawChina.IBLL
-{
-    public interface IBaseBLL
-    {
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
 
+namespace PawChina.IBLL
+{
+    public interface IBaseBLL<T>
+    {
+        #region 基础方法
+
+        #region 查询系列
+        /// <summary>
+        /// 单个值返回值
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="param"></param>
+        /// <param name="transaction"></param>
+        /// <param name="commandTimeout"></param>
+        /// <param name="commandType"></param>
+        /// <returns></returns>
+        Task<T> ExecuteScalarAsync(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = default(int?), CommandType? commandType = default(CommandType?));
+        /// <summary>
+        /// 强类型查询
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="param"></param>
+        /// <param name="transaction"></param>
+        /// <param name="buffered"></param>
+        /// <returns></returns>
+        Task<IEnumerable<T>> QueryAsync(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = default(int?), CommandType? commandType = default(CommandType?));
+        /// <summary>
+        /// 动态类型查询 | 多映射动态查询
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="param"></param>
+        /// <param name="transaction"></param>
+        /// <param name="buffered"></param>
+        /// <returns></returns>
+        Task<IEnumerable<dynamic>> QueryAsyncDynamic(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = default(int?), CommandType? commandType = default(CommandType?));
+        #endregion
+
+        #region 增删改系
+        /// <summary>
+        /// 增删改系
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="param"></param>
+        /// <param name="transaction"></param>
+        /// <param name="commandTimeout"></param>
+        /// <param name="commandType"></param>
+        /// <returns></returns>
+        Task<int> ExecuteAsync(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = default(int?), CommandType? commandType = default(CommandType?));
+        #endregion
+
+        #endregion
+
+        #region 扩展方法
+
+        #region 查询系
+        /// <summary>
+        /// 获取Model-Key为int类型
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="transaction"></param>
+        /// <param name="commandTimeout"></param>
+        /// <returns></returns>
+        Task<T> GetAsync(int id, IDbTransaction transaction = null, int? commandTimeout = null);
+        /// <summary>
+        /// 获取Model-Key为long类型
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="transaction"></param>
+        /// <param name="commandTimeout"></param>
+        /// <returns></returns>
+        Task<T> GetAsync(long id, IDbTransaction transaction = null, int? commandTimeout = null);
+        /// <summary>
+        /// 获取Model-Key为Guid类型
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="transaction"></param>
+        /// <param name="commandTimeout"></param>
+        /// <returns></returns>
+        Task<T> GetAsync(System.Guid id, IDbTransaction transaction = null, int? commandTimeout = null);
+        /// <summary>
+        /// 获取Model集合（没有Where条件）
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<T>> GetAllAsync();
+        #endregion
+
+        #region 增删改
+        /// <summary>
+        /// 插入一个Model
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="transaction"></param>
+        /// <param name="commandTimeout"></param>
+        /// <returns></returns>
+        Task<int> InsertAsync(T model, IDbTransaction transaction = null, int? commandTimeout = null);
+
+        /// <summary>
+        /// 更新一个Model
+        /// </summary>
+        /// <param name="connection"></param>
+        /// <param name="entityToUpdate"></param>
+        /// <param name="transaction"></param>
+        /// <param name="commandTimeout"></param>
+        /// <returns></returns>
+        Task<T> UpdateAsync(T model, IDbTransaction transaction = null, int? commandTimeout = null);
+        #endregion
+
+        #region 分页查询
+        /// <summary>
+        /// 分页查询(为什么不用out，请参考：http://www.cnblogs.com/dunitian/p/5556909.html)
+        /// </summary>
+        /// <param name="sql">查询语句</param>
+        /// <param name="p">动态参数</param>
+        /// <param name="sqlTotal">total语句</param>
+        /// <param name="p2">Total动态参数</param>
+        /// <returns></returns>
+        Task<string> PageLoadAsync(string sql, object p = null, string sqlTotal = "", object p2 = null);
+        #endregion
+
+        #endregion
     }
 }
