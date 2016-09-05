ee1d497eca9f19c664a2c780381424eab224a7fb
dunitian <1054186320@qq.com>
2016-09-05 17:36:45 +0800

    编辑页面添加半自动化操作

---
+++ PawChina/PawChina/PawChina.UI/Areas/PawRoot/assets/js/note.js
@@ -0,0 +1,29 @@
+﻿//人性化提示
+$('#title').keyup(function () {
+    RightMsg(this, 100);
+});
+$('#author').keyup(function () {
+    RightMsg(this, 50);
+});
+
+//半自动化操作
+//默认作者
+$('#autoA').click(function () {
+    $('#author').val('PawChina');
+});
+//随机浏览
+$('#autoB').click(function () {
+    $('#hitCount').val(Math.floor(Math.random() * 1000));
+});
+//重置展图
+$('#autoC').click(function () {
+    $('#displayPic').attr('src', '');
+});
+//清空编辑器
+$('#autoD').click(function () {
+    ue.setContent('');
+});
+//自动分词
+$('#autoE').click(function () {
+
+});
